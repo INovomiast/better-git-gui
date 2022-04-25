0da55e5ce0491e90f0019227af3290331e39404a
inovomiast <inovomiast@hotmail.com>
2022-04-25 11:31:21 +0200

    Working on UUID and Cache Things!

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 const electron = require('electron'); //ELECTRON MODULE IMPORT (Saved in a var to use it later).
-const {app, BrowserWindow, dialog, Notification} = electron; //ELECTRON PARTS IMPORT.
+const {app, BrowserWindow, dialog, Notification, Menu} = electron; //ELECTRON PARTS IMPORT.
 const path = require('path'); //PATH MODULE IMPORT.
 const url = require('url'); //URL MODULE IMPORT.
+
+const fs = require('fs'); //FS MODULE IMPORT.
 
 const os = require('os'); //OS MODULE IMPORT.
 const user_name = os.userInfo().username; //This estract's the device username.
 
+const session_uuid = require('crypto'); //CRYPTO MODULE IMPORT.
+const { stringify } = require('querystring'); //STRINGIFY MODULE IMPORT.
+
 let mainW; //This let... it's the var where the window data is stored.
+
+//Session Manager
+let s_uuid = "" //S_UUID.
+let s_uuid_list = []; //S_UUID_LIST (Here we store all the sessions uuid's).
+
+const gen_uuid = () => {
+    s_uuid = session_uuid.randomUUID(); //Generates the UUID
+    s_uuid_list.push(s_uuid); //Add's the UUID to the list
+    console.log(s_uuid_list); //This show's the session uuid
+}
+
+//Session Manager (UUID Save)
+fs.writeFile('')
+
 
 function mainWCreate(Wwidth, Wheight, WcontextIsolation) { //This funcion is evoked on line:29.
     mainW = new BrowserWindow({ //Creates a new BrowserWindow.
@@ -25,6 +44,20 @@ function mainWCreate(Wwidth, Wheight, WcontextIsolation) { //This funcion is evo
     }));
 }
 
+//Check if is in Dev or Production:
+if(process.env.NODE_ENV === "production") {
+    Menu.setApplicationMenu(null)
+} else if(process.env.NODE_ENV !== "production") {
+    
+}
+
+
 app.whenReady().then(() => { //Check if app is ready.
     mainWCreate(800, 600, false); //CreateMainWindow Function
+    gen_uuid(); //Generates and saves the session_uuid
 });
+
+//Jus't for dev Reasons:
+if(process.env.NODE_ENV !== "production") {
+    console.log(stringify(process.env.NODE_ENV));
+}
